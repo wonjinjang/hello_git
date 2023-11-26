c5fabaf44a2bc85fc282fb97b3dd02e4705c0e56
Wonjin jang <jjwj0228@naver.com>
2023-11-26 01:14:29 +0000

    C4: modify hello.java

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ public class Hello {
    public static void main(String[] args) {
       System.out.println("Hello, world from GIT!")i
       System.out.println("Changes after First commit!");
+      System.out.println("Changes after Pushing to remote!");
    }
 }
